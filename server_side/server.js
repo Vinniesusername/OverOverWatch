d63044a3bc65570bc969008ac32296875d14e666
drew-mack <realdrewmack@gmail.com>
2018-04-22 17:40:39 -0500

    made references to images through the running node server so that they actually work, heading tothe IC

--- server_side/server.js
+++ server_side/server.js
@@ -2,16 +2,15 @@ const http = require('http');
 const express = require('express');
 const path = require('path');
 
-const rel = '/../client_side/html_and_css';
+const rel = '/../client_side/';
 
 const server = express();
-    server.use(express.static(__dirname+rel));
+    server.use(express.static(__dirname+rel+'html_and_css'));
+    server.use(express.static(__dirname+rel+'images'));
+    server.use(express.static(__dirname+rel+'javascript'));
     http.createServer(server).listen(9001);
 
 server.get('/', function(req, res) {
     res.render('index.html')
 });
 
-server.get('/pc', function(req, res) {
-    res.sendFile(express.static('pc.html'))
-});
